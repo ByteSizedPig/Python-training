0de7cf2b9f716f9ad94af51cd4e657ad0b29c11e
ByteSizedPig <michael_sundby@hotmail.com>
2026-08-26 22:03:17 +0200

    added user input+
    user now inputs the target they want to ping.
    also added an alternative way i found to do string interpolation.

--- ipv4 ping.py
+++ ipv4 ping.py
@@ -1,6 +1,9 @@
 import subprocess
 
-def send_ping():
-    return subprocess.call('ping google.com')
+def send_ping(target):
+    return subprocess.call(f'ping {target}')
 
-send_ping()
+def send_ping_legacy_method(target):
+    return subprocess.call('ping {}'.format(target))
+
+send_ping(input("which target would you like to ping?\n"))
